2a7bfe12d2357593534ac27a9e7e7dd36d80d150
Mario Farouk <mariofarouk22@outlook.com>
2025-08-31 16:36:08 +0300

    edit meta date

--- dags/DB2_Dags/SQL/Merge/leads_qualified_merge.sql
+++ dags/DB2_Dags/SQL/Merge/leads_qualified_merge.sql
@@ -1,33 +1,27 @@
 MERGE `ready-de26.project_landing.leads_qualified_mario` T
 USING (
   SELECT 
-    mql_id as lead_id,
-    ANY_VALUE(customer_id) as customer_id,
-    ANY_VALUE(lead_status) as lead_status,
-    ANY_VALUE(CAST(qualified_date AS TIMESTAMP)) as qualified_date,
-    ANY_VALUE(product_interest) as product_interest,
-    ANY_VALUE(CAST(estimated_value AS NUMERIC)) as estimated_value,
-    ANY_VALUE(assigned_rep_id) as assigned_rep_id,
+    mql_id,
+    ANY_VALUE(first_contact_date) as first_contact_date,
+    ANY_VALUE(landing_page_id) as landing_page_id,
+    ANY_VALUE(origin) as origin,
     ANY_VALUE(CAST(updated_at_timestamp AS TIMESTAMP)) as updated_at_timestamp
   FROM `ready-de26.project_landing.leads_qualified_stage_mario`
-  GROUP BY lead_id
+  GROUP BY mql_id
 ) S
-ON T.lead_id = S.lead_id
+ON T.mql_id = S.mql_id
 WHEN MATCHED THEN
   UPDATE SET
-    T.customer_id = S.customer_id,
-    T.lead_status = S.lead_status,
-    T.qualified_date = S.qualified_date,
-    T.product_interest = S.product_interest,
-    T.estimated_value = S.estimated_value,
-    T.assigned_rep_id = S.assigned_rep_id,
+    T.first_contact_date = S.first_contact_date,
+    T.landing_page_id = S.landing_page_id,
+    T.origin = S.origin,
     T.updated_at_timestamp = S.updated_at_timestamp
 WHEN NOT MATCHED THEN
   INSERT (
-    lead_id, customer_id, lead_status, qualified_date,
-    product_interest, estimated_value, assigned_rep_id, updated_at_timestamp
+    mql_id, first_contact_date, landing_page_id, 
+    origin, updated_at_timestamp
   )
   VALUES (
-    S.lead_id, S.customer_id, S.lead_status, S.qualified_date,
-    S.product_interest, S.estimated_value, S.assigned_rep_id, S.updated_at_timestamp
+    S.mql_id, S.first_contact_date, S.landing_page_id, 
+    S.origin, S.updated_at_timestamp
   );
